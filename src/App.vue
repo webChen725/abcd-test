887d2fa3fe0b305c5c45972e245c966cbe67769e
chenguangxue <chenguangxue@bytedance.com>
2021-05-22 16:08:00 +0800

    初始化test project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    Vue2 template(version:1.0.4)
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
